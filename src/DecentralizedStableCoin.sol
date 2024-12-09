3eb7434b96dd708fdd594f26f078218f02c39038
q10357 <isabel@chaparian.no>
2024-12-09 07:22:47 +0100

    modules

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extension/ERC20Burnable.sol";
+
+/*
+ * @title: DecentralizedStableCoin
+ * @author: Patrick "Long Course" Collins (with qTeePie as copy-cat 🐱‍👤💞)
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ */
+contract DecentralizedStableCoin is ERC20Burnable {}
