12da8361e1b841390972df17839081ee87cafda7
q10357 <isabel@chaparian.no>
2025-01-12 16:34:38 +0100

    modules

--- clean_lib.sh
+++ clean_lib.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Cleaning up lib/..."
 git rm -r --cached --staged lib/ -f >/dev/null 2>&1
+git rm -r --cached lib -f
 git restore --staged .gitmodules
 git add .gitmodules
 echo "Done!"
