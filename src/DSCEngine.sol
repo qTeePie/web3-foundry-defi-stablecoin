2fe3b0fd0e7124604208513b52f68db2778b0aa1
q10357 <isabel@chaparian.no>
2025-02-08 12:44:18 +0100

    added tests

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -25,8 +25,6 @@
 
 pragma solidity ^0.8.18;
 
-pragma solidity ^0.8.18;
-
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
